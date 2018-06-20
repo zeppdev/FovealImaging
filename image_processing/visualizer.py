15b93ca04f0d81bd5747bab0d435494acee6f2a6
Adam James <adambarillas@ip-145-116-178-86.wlan-int.ru.nl>
2018-06-20 15:05:55 +0200

    add overlay function

--- image_processing/visualizer.py
+++ image_processing/visualizer.py
@@ -1,5 +1,52 @@
 from matplotlib import pyplot as plt
+import cv2 as cv
 
 def show_image(data):
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+
     plt.imshow(data, cmap='Greys_r')
     plt.show()
+
+
+
+
+def overlay_image(background_img, img_to_overlay_t, x, y, overlay_size=None):
+    """
+    @brief      Overlays a transparant PNG onto another image using CV2
+
+    @param      background_img    The background image
+    @param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
+    @param      x                 x location to place the top-left corner of our overlay
+    @param      y                 y location to place the top-left corner of our overlay
+    @param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
+
+    @return     Background image with overlay on top
+    """
+
+    bg_img = background_img.copy()
+
+    if overlay_size is not None:
+        img_to_overlay_t = cv.resize(img_to_overlay_t.copy(), overlay_size)
+
+    # Extract the alpha mask of the RGBA image, convert to RGB
+    b, g, r, a = cv.split(img_to_overlay_t)
+    overlay_color = cv.merge((b, g, r))
+
+    # Apply some simple filtering to remove edge noise
+    mask = cv.medianBlur(a, 5)
+
+    h, w, _ = overlay_color.shape
+    roi = bg_img[y:y + h, x:x + w]
+
+    # Black-out the area behind the logo in our original ROI
+    img1_bg = cv.bitwise_and(roi.copy(), roi.copy(), mask=cv.bitwise_not(mask))
+
+    # Mask out the logo from the logo image.
+    img2_fg = cv.bitwise_and(overlay_color, overlay_color, mask=mask)
+
+    # Update the original image with our new ROI
+    bg_img[y:y + h, x:x + w] = cv.add(img1_bg, img2_fg)
+
+    return bg_img
